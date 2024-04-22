cd9cbea287b73ad05926c747d669fd5f77438d03
Enzohscc <99258499+Enzohscc@users.noreply.github.com>
2024-04-22 20:19:40 -0300

    feat page home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
+import { AboutComponent } from './domain/pages/about/about.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: async () => (await (import('./domain/pages/inicio/inicio.component'))).InicioComponent
+  },
+  {
+    path: 'about',
+    loadComponent: async () => (await (import('./domain/pages/about/about.component'))).AboutComponent
+  },
+  {
+    path: 'portfolio',
+    loadComponent: async () => (await (import('./domain/pages/portfolio/portfolio.component'))).PortfolioComponent
+  },
+  {
+    path: 'resume',
+    loadComponent: async () => (await (import('./domain/pages/resume/resume.component'))).ResumeComponent
+  },
+  {
+    path: 'contact',
+    loadComponent: async () => (await (import('./domain/pages/contact/contact.component'))).ContactComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
