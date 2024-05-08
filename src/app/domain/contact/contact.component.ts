7b863406452bec9b0f107c18a5ab0dd0de5fd29e
Sustentacao Desenvolvimento <sustentacao@brunsker.com.br>
2024-05-08 11:49:01 -0300

    feat/improvements 2

--- src/app/domain/contact/contact.component.ts
+++ src/app/domain/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TopRightBgComponent } from '../../shared/components/top-right-bg/top-right-bg.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [TopRightBgComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
