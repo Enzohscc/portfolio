8b75cec97569947bdb30ce9626a404b37478813c
Sustentacao Desenvolvimento <sustentacao@brunsker.com.br>
2024-05-13 11:40:59 -0300

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
 import { AuthorContentComponent } from './shared/components/author-content/author-content.component';
+import { AnimationComponent } from './shared/components/animation/animation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent,SideBarComponent,AuthorContentComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SideBarComponent, AuthorContentComponent, AnimationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/components/author-content/author-content.component.ts
+++ src/app/shared/components/author-content/author-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-author-content',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './author-content.component.html',
   styleUrl: './author-content.component.scss'
 })
